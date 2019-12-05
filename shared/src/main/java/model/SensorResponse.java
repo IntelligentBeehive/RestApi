c3aaaea1362dc62f42b4eac2015069d9752911b7
Hugo Mkandawire <h.mkandawire@student.fontys.nl>
2019-12-05 13:10:42 +0100

    [UPDATE] SensorResponse inherits Response

--- shared/src/main/java/model/SensorResponse.java
+++ shared/src/main/java/model/SensorResponse.java
@@ -1,34 +1,7 @@
 package model;
 
-public class SensorResponse {
-    private String operation = "";
-    private String expression = "";
-    private String result = "";
+public class SensorResponse extends Response{
     private Sensor Sensor;
-
-    public String getOperation() {
-        return operation;
-    }
-
-    public void setOperation(String operation) {
-        this.operation = operation;
-    }
-
-    public String getExpression() {
-        return expression;
-    }
-
-    public void setExpression(String expression) {
-        this.expression = expression;
-    }
-
-    public void setResult(String result) {
-        this.result = result;
-    }
-
-    public String getResult() {
-        return result;
-    }
 
     public Sensor getSensor() {
         return Sensor;
