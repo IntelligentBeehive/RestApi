a66bfaf27c6ea1ad4d1a1ddcbc81b385731a7fb4
Hugo Mkandawire <h.mkandawire@student.fontys.nl>
2019-12-05 13:09:01 +0100

    [ADD] abstract class Response for the inheritance

---
+++ shared/src/main/java/model/Response.java
@@ -0,0 +1,35 @@
+package model;
+
+/**
+ * Inheritance response for every responses
+ * @Author Hugo Mkandawire
+ */
+public abstract class Response {
+    protected String operation = "";
+    protected String expression = "";
+    protected String result = "";
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+
+    public String getExpression() {
+        return expression;
+    }
+
+    public void setExpression(String expression) {
+        this.expression = expression;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getResult() {
+        return result;
+    }
+}
